074c8bb5fb671fbf65ceaeffd1f1bd9a3a74bdf4
amesas26293 <amesas26293@columbushs.com>
2025-10-23 11:38:06 -0400

    Finished 1.1

--- Units/Unit 1 - JavaScript Basics/1.1 - JavaScript Intro/Student/src/Lab1_1_JSIntro.js
+++ Units/Unit 1 - JavaScript Basics/1.1 - JavaScript Intro/Student/src/Lab1_1_JSIntro.js
@@ -4,23 +4,35 @@
    =========================================================== */
 
 // TODO 1: Print your full name with console.log()
-
+console.log("Adrian Mesas");
 
 // TODO 2: Declare variable favoriteFood, assign "Steak", then reassign "Pizza"
-
+let favoriteFood = "Steak";
+favoriteFood = "Pizza";
 
 // TODO 3: Create variable mystery, assign a string, then a number, print type each time
-
+let mystery = "Hello";
+console.log("Value:" + mystery + "Data type" + typeof mystery); // prints "string"
+mystery= 42;
+console.log("Value:" + mystery + "Data type" + typeof mystery); // prints "number"
 
 // TODO 4: Declare const schoolName, try to reassign
-
+const schoolName = "Christopher Columbus High School";
 
 // TODO 5: Create three valid camelCase variables and print them
+let quaterBack;
+let pointGaurd;
 
+pointGaurd = 'Dwayne Wade';
+let classRoom = "L12";
 
 // 🚀 Stretch Goals (optional)
 // - Create a variable age and print "I am X years old" using concatenation
 // - Repeat using template literals
+
+let age = 17; //Declared and Initialized
+console.log("I am " + age + " years old"); //Concatenation
+console.log(`I am ${age} years old`); //Template Literals
 
 /* ===========================================================
    🎟 EXIT TICKET
@@ -28,3 +40,16 @@
    2. What challenges did you encounter, and how did you solve them?
    3. Where else could this programming concept be useful?
    =========================================================== */
+
+
+   // Mini FRQ - Lab 1.1
+   //Declaring and Initializing
+   let width = 5;
+   let height = 10;
+   
+    // Computing Area and Perimeter
+   let area = width * height;
+   let perimeter = 2 * (width + height);
+
+   console.log("The area is:" + area);
+   console.log("The perimeter is:" + perimeter);
