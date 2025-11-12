5fd65908fd42c58fe3cdb4f6709ed44212349f10
amesas26293 <amesas26293@columbushs.com>
2025-11-12 11:25:09 -0500

    Finished 1.4

--- Units/Unit 1 - JavaScript Basics/1.4_ConditionalsLoopsLab/Student/1.4.1/src/Lab1_4_ConditionalsLoops.js
+++ Units/Unit 1 - JavaScript Basics/1.4_ConditionalsLoopsLab/Student/1.4.1/src/Lab1_4_ConditionalsLoops.js
@@ -3,75 +3,81 @@
 */
 
 // TODO 1: if..else with age discounts
-function getDiscountedPrice(age, price) {
-    if (age < 12) {
-        return price * 0.5; // 50% discount for children
-    } else if (age >= 65) {
-        return price * 0.7; // 30% discount for seniors
-    } else {
-        return price; // no discount
+let age = 15;
+if(age < 16) {
+    console.log ("Teen Discount");
+} else if (age <= 65) {
+    console.log("Senior Discount");
+} else {
+       console.log ("No discount")
     }
+
+let weight = 180;
+if(weight <= 150) {
+    console.log ("Your Slim");
+} else if (weight <= 190) {
+    console.log("You are in shape");
+} else {
+    console.log("You need to start working out");
 }
+    
+
 
 // TODO 2: Nested if with two numbers
-function compareNumbers(num1, num2) {
-    if (num1 > num2) {
-        return "First is greater";
-    } else if (num1 < num2) {
-        return "Second is greater";
-    } else {
-        return "Both are equal";
-    }
+let num1 = 10;
+let num2 = 20;
+
+if(num1 > num2) {
+    console.log("num1 is greater");
+} else if(num1 < num2) {
+    console.log("num2 is greater");
+} else {
+    console.log("Both numbers are equal");
 }
+
+
 
 // TODO 3: Switch statement for language greeting
-function getGreeting(language) {
-    switch (language) {
-        case "English":
-            return "Hello!";
-        case "Spanish":
-            return "¡Hola!";
-        case "French":
-            return "Bonjour!";
-        case "German":
-            return "Hallo!";
-        default:
-            return "Hello!";
-    }
+let language = "French";
+switch(language) {
+    case "Spanish":
+        console.log("Hola, mundo!");
+        break;
+    case "French":
+        console.log("Bonjour, le monde!");
+        break;
+    default:
+        console.log("Hello, world!");
 }
+
+let stock = "APPL"
+switch(stock) {
+    case "APPL":
+    case "GOOG":
+    case "NVDA":
+        console.log("Technology");
+        break;
+    case "JNJ": 
+    case "UNH":
+    case "VRTX":
+        console.log("Healthcare");  
+        break;
+    
+    
+}
+
 
 // TODO 4: While loop (1 to 10)
-function printOneToTen() {
-    let i = 1;
-    while (i <= 10) {
-        console.log(i);
-        i++;
-    }
-}
 
 // TODO 5: Do..while loop (1 to 5)
-function printOneToFive() {
-    let i = 1;
-    do {
-        console.log(i);
-        i++;
-    } while (i <= 5);
-}   
+
 
 // TODO 6: For loop (1 to 20, print "ten" at 10 and "twenty" at 20)
-function printOneToTwenty() {
+
 
 
 // TODO 7: Even/Odd loop (1 to 20)
-function printEvenOdd() {
-    for (let i = 1; i <= 20; i++) {
-        if (i % 2 === 0) {
-            console.log(i + " is even");
-        } else {
-            console.log(i + " is odd");
-        }
-    }
-}
+
 
 // 🚀 Stretch Goals
 // - Backwards loop from 20 to 1
