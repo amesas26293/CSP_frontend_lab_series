ee23851faeccc126fd6b51866372941de0da9189
amesas26293 <amesas26293@columbushs.com>
2025-11-12 11:36:50 -0500

    Finished T2

--- Units/Unit 1 - JavaScript Basics/1.4_ConditionalsLoopsLab/Student/1.4.2/Practice_Activities/Tier02_Loops/Tier02_Loops.js
+++ Units/Unit 1 - JavaScript Basics/1.4_ConditionalsLoopsLab/Student/1.4.2/Practice_Activities/Tier02_Loops/Tier02_Loops.js
@@ -1,2 +1,12 @@
 // Your code goes here. Follow the steps in README.md and run from index.html.
 console.log("TEST - TIER 2")
+for (let i= 1; i <= 10; i++) {
+    console.log(i);
+}
+
+let countdown = 10;
+while (countdown > 0) {
+    console.log(countdown);
+    countdown--;
+}
+console.log("Liftoff!");
