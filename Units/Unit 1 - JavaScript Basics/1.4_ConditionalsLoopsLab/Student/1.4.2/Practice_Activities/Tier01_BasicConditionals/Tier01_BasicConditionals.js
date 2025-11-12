bdb912621762caab89023e13dbae5c9830682151
amesas26293 <amesas26293@columbushs.com>
2025-11-12 11:25:20 -0500

    Finished T1

--- Units/Unit 1 - JavaScript Basics/1.4_ConditionalsLoopsLab/Student/1.4.2/Practice_Activities/Tier01_BasicConditionals/Tier01_BasicConditionals.js
+++ Units/Unit 1 - JavaScript Basics/1.4_ConditionalsLoopsLab/Student/1.4.2/Practice_Activities/Tier01_BasicConditionals/Tier01_BasicConditionals.js
@@ -1,2 +1,20 @@
 // Your code goes here. Follow the steps in README.md and run from index.html.
 console.log("TEST - TIER 1")
+
+let number = -5;
+if (Number.isNaN(number)) {
+    console.log("Invalid number");
+} else if (n % 2 === 0) {
+    console.log("Even number");
+} else {
+    console.log("Odd number");
+}
+
+let age= 25;
+if (age >= 18)
+{
+    console.log("You can vote!");
+}
+if (age < 18) {
+    console.log("You are too young to vote");
+}
